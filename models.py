6d48c52d7752f4083bf80589c66b8e9652859f76
ibrahimsandouk <ibrahim.sandouk@gmail.com>
2019-12-05 19:13:37 +0500

    add methods to api

--- models.py
+++ models.py
@@ -9,3 +9,13 @@ class Men (db.Model):
 
     def json(self):
          return {"id":self.id, "name":self.name}
+
+class Building (db.Model2):
+    __tablename__='Building'
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(120))
+    space = db.Column (db.Integer())
+    floorscount = db.column (db.string(160))
+
+    def json(self):
+         return {"id":self.id, "address":self.address, "space":self.space, "floorscount":self.floorscount}
