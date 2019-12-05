13535a04497971c637ba1cc6a80310275ab45c51
ibrahimsandouk <ibrahim.sandouk@gmail.com>
2019-12-05 19:16:43 +0500

    deploy to heroku

--- models.py
+++ models.py
@@ -1,21 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy ()
+db = SQLAlchemy()
 
-class Men (db.Model):
-    __tablename__='mens'
+
+class Men(db.Model):
+    __tablename__ = 'mens'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
 
     def json(self):
-         return {"id":self.id, "name":self.name}
+        return {"id": self.id, "name": self.name}
 
-class Building (db.Model2):
-    __tablename__='Building'
+
+class Building(db.Model):
+    __tablename__ = 'Building'
     id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(120))
-    space = db.Column (db.Integer())
-    floorscount = db.column (db.string(160))
+    space = db.Column(db.Integer())
+    floorscount = db.column(db.string(160))
 
     def json(self):
-         return {"id":self.id, "address":self.address, "space":self.space, "floorscount":self.floorscount}
+        return {"id": self.id, "address": self.address, "space": self.space, "floorscount": self.floorscount}
